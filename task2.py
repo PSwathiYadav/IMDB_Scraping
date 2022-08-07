53f95bd6ab1c7f363eb35a0cfde3a609e7e94587
PSwathiYadav <swathip21@navgurukul.org>
2022-08-07 12:20:31 +0530

    webscraping

---
+++ task2.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests , json
+from pprint import pprint
+
+file2=open("all_moviesT1.json","r")
+movies=json.load(file2)
+
+def group_by_year(movies):
+    dict1={}
+    for i in movies:
+        # print(i)
+        y=i["year"]
+        if y not in dict1:
+            dict1[y]=[]
+        else:
+            dict1[y].append(i)
+            # pprint(dict1)
+    with open('yearwise_movieT2.json','w') as f3:
+        json.dump(dict1,f3,indent=4)
+        return dict1       
+pprint(group_by_year(movies))
